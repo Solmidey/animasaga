944154ec38adb2fd5c0a8316c8320a1f29ab0159
Solmidey <122814155+Solmidey@users.noreply.github.com>
2026-01-17 13:06:05 +0100

    Implement ElyndraCommitment contract
    Added ElyndraCommitment contract with faction selection.

--- contracts/elyndra/ElyndraCommitment.sol
+++ contracts/elyndra/ElyndraCommitment.sol
@@ -1,1 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "../core/SagaCommit.sol";
+
+contract ElyndraCommitment is SagaCommit {
+    enum Faction {
+        Flame,
+        Veil,
+        Echo,
+        Crown
+    }
+
+    mapping(address => Faction) public factionChoice;
+
+    event FactionChosen(address indexed user, Faction faction);
+
+    constructor(address registryAddress)
+        SagaCommit(registryAddress)
+    {}
+
+    function chooseFaction(Faction faction) external {
+        bytes32 commitmentHash = keccak256(
+            abi.encodePacked(msg.sender, faction, block.number)
+        );
+
+        // Saga 1, Season 1 (hardcoded intentionally)
+        commit(commitmentHash, 1, 1);
+
+        factionChoice[msg.sender] = faction;
+
+        emit FactionChosen(msg.sender, faction);
+    }
+}
 
